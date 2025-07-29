e75502a0e6429f15ba1f7acdf10e77f7d35aba46
Abraham Addo <addoabraham@gmail.com>
2025-07-29 14:52:12 +0000

    advanced

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -28,7 +28,7 @@ const Search = () => {
         setSingleUser(user);
       }
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
